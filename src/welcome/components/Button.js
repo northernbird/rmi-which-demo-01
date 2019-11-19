379422fec8321504cdf4dab5e5e7b14841ca4e85
Rie Miyazaki <rie.miyazaki@sprocket.bz>
2019-11-19 23:26:18 +0900

    WIP demo initial checkin

---
+++ src/welcome/components/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+    button: {
+        background: 'linear-gradient(135deg, #2efdf7, #0f8bfd)',
+        border: 0,
+        borderRadius: '20px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0px 0px 0px 0px',
+        margin: '160px 0px 0px 0px',
+        width: '270px',
+        'font-size': 14,
+        'font-weight': 700,
+        letterSpacing: '0.2em'
+    },
+});
+
+function WelcomeButton() {
+    const classes = useStyles();
+    return (
+        <Button className={classes.button}>
+            はじめる
+        </Button>
+    );
+}
+
+export default WelcomeButton;
