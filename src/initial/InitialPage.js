1c7c4cd40983bdb76bbfef9a6085efa3afce0f67
Rie Miyazaki <miyazaki.rie98@gmail.com>
2019-12-02 20:17:33 +0900

    Added new initial question page

--- src/initial/InitialPage.js
+++ src/initial/InitialPage.js
@@ -3,36 +3,62 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-    pannel: {
-        alignContent: 'center',
-        alignItems: 'center',
-        background: '#ffffff',
-        marginTop: '230px',
-        marginRight: '0px',
-        marginBottom: '0px',
-        marginLeft: '0px',
-        width: '100%',
-        maxWidth: '100%',
-        height: '380px',
-        textAlign: 'center',
+    StudioCanvas: {
+        height: 'auto',
+        'min-height': '100vh',
     },
-    title: {
-        textAlign: 'center',
-        width: 'auto',
-        maxWidth: '100%',
+    flexPanel: {
+        'z-index': '0',
+        'flex-wrap': 'nowrap',
+        display: 'flex',
+        position: 'relative',
+        'flex-direction': 'column',
+        'align-content': 'center',
+        'align-items': 'center',
     },
-    subTitle: {
-        marginTop: '46px',
-        marginRight: '0px',
-        marginBottom: '0px',
-        marginLeft: '0px',
+    titleBox: {
+        display: 'flex',
+        'align-items': 'center',
+        'background': '#ffffff',
+        'border': 'solid 1px #ff0000',
+        'border-radius': '10px',
+        'flex': 'none',
+        'height': '70px',
+        'justify-content': 'center',
+        'margin': '30px 0px 0px 0px',
+        'width': '80%',
+        'max-width': '80%',
+        '-webkit-box-align': 'center'
+    },
+    titleContent: {
+        'color': '#ff0000',
+        'font-family': 'Lato',
+        'font-size': '14px',
+        'font-weight': '700',
+        'height': 'auto',
+        'letter-spacing': '0.05em',
+        'line-height': '1.4',
+        'margin': '0px 0px 0px 0px',
+        'text-align': 'center',
+        'width': 'auto',
+        'max-width': '100%',
+        'justify-content': 'center',
     }
+
 });
 
 function InitialPage(props) {
+    const classes = useStyles()
     return (
-        <Typography component="div" >
-            Hello Test Question World
+        <Typography component="div"  className={classes.StudioCanvas}>
+            <div className={classes.flexPanel}>
+                <div className= {classes.titleBox}>
+                    <p className={classes.titleContent}>
+                    テスト用の質問です。<br/>
+                    気軽にどちらか選択してみましょう。
+                    </p>
+                </div>
+            </div>
         </Typography>
     );
 }
