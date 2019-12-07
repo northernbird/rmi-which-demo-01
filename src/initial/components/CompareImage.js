d71114fbf48e4d2f3ff43d9677a944f27ff4f52c
Rie Miyazaki <miyazaki.rie98@gmail.com>
2019-12-08 08:18:41 +0900

    Added new initial question page

--- src/initial/components/CompareImage.js
+++ src/initial/components/CompareImage.js
@@ -30,7 +30,9 @@ const useStyles = makeStyles({
         'align-items': 'center',
         'overflow': 'hidden'
     },
-    iconDiv: {
+    // TODO apply responsive design (especially button size)
+    // TODO use common method for rightIcon & leftIcon
+    rightIconDiv: {
         'background': 'rgba(0, 0, 0, 0.5)',
         'border-radius': '50%',
         // 'bottom': '30px',
@@ -65,6 +67,44 @@ const useStyles = makeStyles({
         // 'word-wrap': 'normal',
         // 'direction': 'ltr',
     },
+    // TODO apply responsive design (especially button size)
+    // TODO use common method for rightIcon & leftIcon
+    leftIconDiv: {
+        'background': 'rgba(0, 0, 0, 0.5)',
+        'border-radius': '50%',
+        // 'bottom': '30px',
+        'position': 'absolute',
+        'height': '35px',
+        'width': '35px',
+        // 'height': '9%',
+        // 'width': '3%',
+        // padding: '5% 5% 5% 5%',
+        'left': '26px',
+        'margin': '70% 0% 0% 0%',
+        'display': 'flex',
+        'align-items': 'center',
+        'justify-content': 'center',
+        'flex-direction': 'row',
+        'right': 'auto',
+
+        // 'top': 'auto',
+        // 'max-width': '100%',
+        // '-webkit-box-orient': 'horizontal',
+        // '-webkit-box-direction': 'normal',
+        // '-webkit-box-align': 'center',
+        // 'align-content': 'center',
+        // '-webkit-box-pack': 'center',
+        // 'overflow': 'visible',
+        // 'font-weight': 'normal',
+        // 'font-style': 'normal',
+        // 'line-height': '1',
+        // 'letter-spacing': 'normal',
+        // 'text-transform': 'none',
+        // 'white-space': 'nowrap',
+        // 'word-wrap': 'normal',
+        // 'direction': 'ltr',
+    },
+
     icon: {
         'position': 'relative',
         'color': '#dfdfdf',
@@ -76,10 +116,14 @@ const useStyles = makeStyles({
 
 function CompareImage(props) {
     const classes = useStyles()
+    const magnify = () => alert("AAA")
     return (
         <div className={classes.sdPannel}>
             <img src="https://storage.googleapis.com/production-os-assets/assets/24e1851f-25ac-4b52-96a4-467073e181e9" className={classes.sd} />
-            <div className={classes.iconDiv}>
+            <div className={classes.rightIconDiv} onClick={magnify}>
+                <SearchIcon className={classes.icon}/>
+            </div>
+            <div className={classes.leftIconDiv} onClick={magnify}>
                 <SearchIcon className={classes.icon}/>
             </div>
         </div>
