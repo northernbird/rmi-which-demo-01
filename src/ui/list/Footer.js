6cc22dd7b654d90eda764455f5fa37203bc2f8da
Rie Miyazaki <miyazaki.rie98@gmail.com>
2019-12-27 08:20:20 +0900

    Added postform button

--- src/ui/list/Footer.js
+++ src/ui/list/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import IconButton from "@material-ui/core/IconButton";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
 const useStyles = makeStyles({
     pannel: {
@@ -12,25 +13,54 @@ const useStyles = makeStyles({
         height: '100%',
         display: 'flex'
     },
-    footer: {
-        display: 'flex',
-        position: 'absolute',
-        bottom: '0px',
-        top: 'auto',
-        height: '20%',
-        background: '#FFFFFF',
-    },
+    // footer: {
+    //     display: 'flex',
+    //     position: 'absolute',
+    //     bottom: '0px',
+    //     top: 'auto',
+    //     height: '20%',
+    //     background: '#FFFFFF',
+    // },
     button: {
         fontSize: 40,
+    },
+    submitButton : {
+        'align-content': 'center',
+        'align-items': 'center',
+        'background': 'linear-gradient(135deg, #2efdf7, #0f8bfd)',
+        'border-radius': '50%',
+        'box-shadow': '0 2px 5px rgba(0,0,0,0.2)',
+        'flex': 'none',
+        'flex-direction': 'column',
+        'flex-wrap': 'nowrap',
+        'height': '60px',
+        'justify-content': 'center',
+        'margin': '5px 0px 0px 0px',
+        'padding': '0px 0px 0px 0px',
+        'width': '50px',
+        // 'max-width': '100%'
+    },
+    postFormButton: {
+        'background': 'linear-gradient(135deg, #2efdf7, #0f8bfd)',
+        'color': '#000000000'
     }
+
 });
 
 function Footer(props) {
     const classes = useStyles()
     return (
         <div className={classes.pannel}>
-            <IconButton className={classes.button}>
+            <IconButton>
                 <HomeRoundedIcon className={classes.button} />
+            </IconButton>
+            <IconButton>
+                <BookmarkBorderIcon className={classes.button} />
+            </IconButton>
+            <IconButton>
+                <Fab color="secondary" className={classes.postFormButton} >
+                    <AddIcon />
+                </Fab>
             </IconButton>
         </div>
     );
