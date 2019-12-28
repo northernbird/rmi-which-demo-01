45c0f431eac3502afa748b96ed7fbd38926832e7
Rie Miyazaki <miyazaki.rie98@gmail.com>
2019-12-28 12:45:38 +0900

    wip for layout

--- src/ui/add/components/AddCategoryPannel.js
+++ src/ui/add/components/AddCategoryPannel.js
@@ -8,10 +8,16 @@ const useStyles = makeStyles({
     pannel: {
         'display': 'flex',
         'flex-flow': 'row nowrap',
-        'justify-content': 'space-around',
-        'height': '13vh',
+        'justify-content': 'center',
+        'height': '15vh',
         'align-items': 'center',
 
+    },
+    categoryPannel: {
+        'display':'flex',
+        'flex-flow': 'column nowrap',
+        'align-items': 'center',
+        'width': '30%'
     },
     imagePannel: {
         'align-content': 'center',
@@ -25,7 +31,17 @@ const useStyles = makeStyles({
         'width': '40px',
         'max-width': '100%',
         'display':'flex',
-        'flex-flow': 'row nowrap',
+    },
+    label: {
+        'color': '#333',
+        'font-size': '10px',
+        'font-weight': '400',
+        'height': 'auto',
+        'margin': '10px 0px 0px 0px',
+        'text-align': 'center',
+        'width': 'auto',
+        'max-width': '100%',
+        'justify-content': 'center'
     },
     image: {
         position: 'relative',
@@ -40,20 +56,35 @@ function AddCategoryPannel() {
 
     return (
  <div className={classes.pannel}>
-     <div className={classes.imagePannel} >
-         <img className={classes.image} src="https://storage.googleapis.com/production-os-assets/assets/72de7e7c-ac10-41ce-8e3b-e9054ae03635"/>
+     <div className={classes.categoryPannel}>
+         <div className={classes.imagePannel} >
+             <img className={classes.image} src="https://storage.googleapis.com/production-os-assets/assets/72de7e7c-ac10-41ce-8e3b-e9054ae03635"/>
+         </div>
+         <p className={classes.label}>ファッション</p>
      </div>
-     <div className={classes.imagePannel} >
-         <img className={classes.image} src="/content-cut.png"/>
+     <div className={classes.categoryPannel}>
+         <div className={classes.imagePannel} >
+             <img className={classes.image} src="/content-cut.png"/>
+         </div>
+         <p className={classes.label}>ヘア</p>
      </div>
-     <div className={classes.imagePannel} >
-         <img className={classes.image} src="https://storage.googleapis.com/production-os-assets/assets/1001baa8-953d-40ad-9ca5-29a30ecc3d3a"/>
+     <div className={classes.categoryPannel}>
+         <div className={classes.imagePannel} >
+             <img className={classes.image} src="https://storage.googleapis.com/production-os-assets/assets/1001baa8-953d-40ad-9ca5-29a30ecc3d3a"/>
+         </div>
+         <p className={classes.label}>プレゼント</p>
      </div>
-     <div className={classes.imagePannel} >
-         <img className={classes.image} src="https://storage.googleapis.com/production-os-assets/assets/7a42776e-bbcf-4bb2-8f9c-8fd3dac31e27"/>
+     <div className={classes.categoryPannel}>
+         <div className={classes.imagePannel} >
+             <img className={classes.image} src="https://storage.googleapis.com/production-os-assets/assets/7a42776e-bbcf-4bb2-8f9c-8fd3dac31e27"/>
+         </div>
+         <p className={classes.label}>メイク</p>
      </div>
-     <div className={classes.imagePannel} >
-         <p>etc..</p>
+     <div className={classes.categoryPannel}>
+         <div className={classes.imagePannel} >
+             <p>etc..</p>
+         </div>
+         <p className={classes.label}>その他</p>
      </div>
  </div>
     );
