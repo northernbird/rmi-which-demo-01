48420ae20ce83af3ba44e026524d4c9e6c842531
Rie Miyazaki <miyazaki.rie98@gmail.com>
2019-12-28 13:39:57 +0900

    wip for layout

--- src/ui/common/Footer.js
+++ src/ui/common/Footer.js
@@ -16,6 +16,7 @@ const useStyles = makeStyles({
         display: 'flex',
         bottom: '0px',
         background: '#FFFFFF',
+        'justify-content': 'space-around',
     },
     // footer: {
     //     display: 'flex',
