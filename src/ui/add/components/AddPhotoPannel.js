808d2614a787bbbd4c4bb0591e33494c5e1cb0fb
rmi <rie.miyazaki98@gmail.com>
2020-01-28 11:50:21 +0900

    Refactored image uploader

--- src/ui/add/components/AddPhotoPannel.js
+++ src/ui/add/components/AddPhotoPannel.js
@@ -4,6 +4,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import {useDropzone} from "react-dropzone";
 import Box from "@material-ui/core/Box";
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import ImageUpload from "../../register/components/ImageUpload";
 
 const useStyles = makeStyles({
     pannel: props => ({
@@ -47,39 +48,32 @@ function InputQuestionTextPannel() {
     }
     const classes = useStyles(props)
 
+    const renderContent = (isDragActive) =>  {
+        return [(
+            isDragActive ? <Box letterSpacing={'0.1em'} fontSize={12} fontWeight={700} color={'#686868'}>画像をドロップ ...</Box> :
+                <div>
+                    <Box display="flex" flexDirection="column" justifyContent="center" letterSpacing={'0.1em'} fontSize={12} fontWeight={700} color={'#686868'}>
+                        <Box display="flex" justifyContent="center">
+                            <InsertDriveFileIcon color={'#686868'}/>
+                        </Box>
+                        <Box>
+                            画像を追加
+                        </Box>
+                    </Box>
+                </div>
+        )]
+    };
+
+
     return (
         <div className={classes.pannel}>
             <div {...getRootProps()} className={classes.photoPannel}>
                 <input {...getInputProps()} />
-                {isDragActive ?
-                    <Box letterSpacing={'0.1em'} fontSize={12} fontWeight={700} color={'#686868'}>画像をドロップ ...</Box> :
-                    <div>
-                        <Box display="flex" flexDirection="column" justifyContent="center" letterSpacing={'0.1em'} fontSize={12} fontWeight={700} color={'#686868'}>
-                            <Box display="flex" justifyContent="center">
-                                <InsertDriveFileIcon color={'#686868'}/>
-                            </Box>
-                            <Box>
-                                画像を追加
-                            </Box>
-                        </Box>
-                    </div>
-                }
+                {renderContent(isDragActive)}
             </div>
             <div className={classes.photoPannel}>
                 <input {...getInputProps()} />
-                {isDragActive ?
-                    <Box letterSpacing={'0.1em'} fontSize={12} fontWeight={700} color={'#686868'}>画像をドロップ ...</Box> :
-                    <div>
-                        <Box display="flex" flexDirection="column" justifyContent="center" letterSpacing={'0.1em'} fontSize={12} fontWeight={700} color={'#686868'}>
-                            <Box display="flex" justifyContent="center">
-                                <InsertDriveFileIcon color={'#686868'}/>
-                            </Box>
-                            <Box>
-                                画像を追加
-                            </Box>
-                        </Box>
-                    </div>
-                }
+                {renderContent(isDragActive)}
             </div>
         </div>
     );
